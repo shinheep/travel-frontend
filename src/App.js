656e3d0ef1a900b7efdfd49a2e3e89ec5285ad07
“Alex <alexandriadelaney923@gmail.com>
2021-11-19 15:58:15 -0800

    changed to components on app.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Feed from './Components/Feed';
 import Footer from './Components/Footer';
 import Nav from './Components/Nav';
 import TeamPage from './Components/TeamPage';
-import { Route, Routes } from 'react-router';
+// import { Route, Routes } from 'react-router';
 
 function App() {
 	return (
 		<div className='App'>
 			<Nav />
-			<Routes>
-				<Route path='/' element={<SignUp />} />
-				<Route path='/' element={<Login />} />
-				<Route path='/' element={<CreatePost />} />
-				<Route path='/' element={<Feed />} />
-				<Route path='/' element={<TeamPage />} />
-				<Route path='/' element={<Footer />} />
-			</Routes>
+			{/* <Routes> */}
+				<SignUp/>
+				<Login/>
+				<CreatePost/>
+				<Feed/>
+				<TeamPage/>
+				<Footer/>
+			{/* </Routes> */}
 		</div>
 	);
 }
