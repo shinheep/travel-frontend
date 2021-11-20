69da9c655cca8223fc1ab2493fd1180a4c36f68d
orawee chan <oraweechan@orawees-MacBook-Air.local>
2021-11-19 16:56:35 -0800

    added mui

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,41 @@
+import { Container, Col, Row } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+
 function Login() {
-	return (
-		<div className='Login'>
-			<h1>Login</h1>
-		</div>
-	);
+  return (
+    <>
+	<h1>Login</h1>
+      <Container>
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Email
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue="email@example.com"
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formPlaintextPassword"
+          >
+            <Form.Label column sm="2">
+              Password
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+        </Form>
+      </Container>
+    </>
+  );
 }
 
 export default Login;
