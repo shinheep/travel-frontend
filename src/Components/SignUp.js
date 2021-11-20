0b3ced2ff0bf72df1a3aac92e3a36429720a95f3
orawee chan <oraweechan@orawees-MacBook-Air.local>
2021-11-19 16:31:47 -0800

    signup form

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,10 +1,40 @@
+import { Container, Col, Row } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 function SignUp() {
-	return (
-		<div className='SignUp'>
-			<h1>Sign up</h1>
-		</div>
-	);
+  return (
+    <>
+      <Container>
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Email
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control
+                plaintext
+                readOnly
+                defaultValue="email@example.com"
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formPlaintextPassword"
+          >
+            <Form.Label column sm="2">
+              Password
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+        </Form>
+      </Container>
+    </>
+  );
 }
 
 export default SignUp;
