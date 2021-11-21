841b8876d9a13dc7004fa21317bd1a6ea03a5797
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-11-20 22:37:51 -0800

    create post button functional

--- src/Components/CreatePost.js
+++ src/Components/CreatePost.js
@@ -1,27 +1,45 @@
-// import { Card, Button } from "react-bootstrap";
-import '../createPost.css'
+import React, {useState} from 'react';
+import '../createPost.css';
 
 function CreatePost() {
-  return (
+	const [post, setPost] = useState({username: '', img: '', location: '', price: '', caption: ''})
+
+	const handleChange = (event) => {
+		const value = event.target.value;
+		const name = event.target.name;
+		const copy = Object.assign({}, post);
+		copy[name] = value
+		setPost(copy)
+	}
+	
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(event);
+		console.log("Travel data submitted!");
+		fetch("https://travelgram-app-heroku.herokuapp.com/posts", {
+			method: "POST",
+			headers: {
+				"Content-Type": "Application/json",
+			  },
+			body: JSON.stringify(post),
+		})
+		.then(response => response.json())
+		.then(data => setPost({username: '', img: '', location: '', price: '', caption: ''}))
+	}
+
+	return (
     <div className="CreatePost">
       <h1>Create Post</h1>
       <div className="newPostContainer">
-        {/* <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card> */}
 		<div className="newPostHeader"> Create New Post</div>
-		<div className="newPostUrl">Pic</div>
-		<div className="Destination">Destination</div>
-		<div className="Caption">Caption</div>
-		<button>Post</button>
+		<form onSubmit={handleSubmit}>
+		<div><input onChange={handleChange} name="username" type="text" placeholder="Username" value={post.username} /></div>
+			<div><input onChange={handleChange} name="img" type="text" placeholder="Image URL" value={post.img} /></div>
+			<div><input onChange={handleChange} name="location" type="text" placeholder="Location" value={post.location}/></div>
+			<div><input onChange={handleChange} name="price" type="text" placeholder="Price" value={post.price}/></div>
+			<div><input onChange={handleChange} name="caption" type="text" placeholder="Caption" value={post.caption}/></div>
+			<div><button type="submit">Post</button></div>
+		</form>
       </div>
     </div>
   );
