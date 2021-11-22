cc8347359580d73b0de2a64a15890181901315f6
orawee chan <oraweechan@orawees-MacBook-Air.local>
2021-11-22 15:24:09 -0800

    login and signup functionality

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,43 +2,49 @@ import { Container, Col, Row } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import Button from "@mui/material/Button";
 
-
 function Login() {
   return (
     <>
-	<h1>Login</h1>
       <Container>
-        <Form>
-          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-            <Form.Label column sm="2">
-              Email
-            </Form.Label>
-            <Col sm="5">
-              <Form.Control
-                plaintext
-                readOnly
-                defaultValue="email@example.com"
-              />
-            </Col>
-          </Form.Group>
+        <Row className="justify-content-md-center">
+          <Col sm={3}>
+            <h1>Login</h1>
+            <Form>
+              <Form.Group
+                as={Row}
+                className="mb-3"
+                controlId="formPlaintextEmail"
+              >
+                <Form.Label column sm="4">
+                  Email
+                </Form.Label>
+                <Col sm="12">
+                  <Form.Control placeholder="Email" />
+                </Col>
+              </Form.Group>
 
-          <Form.Group
-            as={Row}
-            className="mb-3"
-            controlId="formPlaintextPassword"
-          >
-            <Form.Label column sm="2">
-              Password
-            </Form.Label>
-            <Col sm="5">
-              <Form.Control type="password" placeholder="Password" />
-            </Col>
-          </Form.Group>
-          <Button variant="outlined" size="large">Login</Button>
-        </Form>
+              <Form.Group
+                as={Row}
+                className="mb-3"
+                controlId="formPlaintextPassword"
+              >
+                <Form.Label column sm="4">
+                  Password
+                </Form.Label>
+                <Col sm="12">
+                  <Form.Control type="password" placeholder="Password" />
+                </Col>
+              </Form.Group>
+              <Button style={{backgroundColor: '#05668d'}} variant="contained" size="large">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     </>
   );
 }
 
 export default Login;
+
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,57 +1,111 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { Container } from "react-bootstrap";
-// import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
 
 function SignUp() {
-  // const [accountInfo, setAccountInfo] = useState({ email: "", name: "",password:"" });
+  const [user, setUser] = useState({
+    email: "",
+    name: "",
+    password: "",
+    username:"",
+  });
+
+
+  const handleChange = (event) => {
+    // console.log(event)
+    const value = event.target.value;
+    const name = event.target.name;
+    const copy = Object.assign({}, user);
+    copy[name] = value;
+    setUser(copy);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("User data submitted!");
+    fetch("http://localhost:8080/users/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        setUser({ email: "", name: "", password: "", username: ""})
+      );
+  };
+  
+
 
   return (
     <>
-      <h1>Sign Up</h1>
+      
       <Container>
-        <Box
-          component="form"
-          sx={{
-            "& .MuiTextField-root": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <div>
-            <TextField
-              className="outlined-textarea"
-              label="Email"
-              placeholder="Email"
-              multiline
-            />
-            <TextField
-              className="outlined-textarea"
-              label="Full Name"
-              placeholder="Full Name"
-              multiline
-            />
-            <TextField
-              className="outlined-textarea"
-              label="Username"
-              placeholder="Username"
-              multiline
-            />
-            <TextField
-              className="outlined-password-input"
-              type="password"
-              label="Password"
-              autoComplete="current-password"
-            />
-            <Button variant="outlined" size="large">
-              Sign up
-            </Button>
-          </div>
-        </Box>
+      
+        <Row className="justify-content-md-center">
+          <Col sm={3} lg={3}>
+          <h1>Sign Up</h1>
+            <Box
+            justifyContent="center"
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+              onSubmit={handleSubmit}
+            >
+                <TextField
+                  className="outlined-textarea"
+                  label="Email"
+                  placeholder="Email"
+                  multiline
+                  onChange={handleChange}
+                  value={user.email}
+                  name="email"
+                />
+                <TextField
+                  className="outlined-textarea"
+                  label="Full Name"
+                  placeholder="Full Name"
+                  multiline
+                  onChange={handleChange}
+                  value={user.name}
+                  name="name"
+                />
+                <TextField
+                  className="outlined-textarea"
+                  label="Username"
+                  placeholder="Username"
+                  multiline
+                  onChange={handleChange}
+                  value={user.username}
+                  name="username"
+                />
+                <TextField
+                  className="outlined-password-input"
+                  type="password"
+                  label="Password"
+                  autoComplete="current-password"
+                  onChange={handleChange}
+                  value={user.password}
+                  name="password"
+                />
+              
+                  <Button style={{backgroundColor: '#05668d'}} type="submit" variant="contained" size="large">
+                    Sign up
+                  </Button>
+               
+            </Box>
+          </Col>
+        </Row>
       </Container>
     </>
   );
 }
 
 export default SignUp;
+
