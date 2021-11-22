30d1d572ddb7becb58611927056a97645bed9a5e
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-11-22 12:08:58 -0800

    fixed import bugs

--- src/Components/CreatePost/CreatePost.js
+++ src/Components/CreatePost/CreatePost.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import './createPost.css';
 
 function CreatePost() {
