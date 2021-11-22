13463f66e8e8b51c48439f77f6c55dc635616046
orawee chan <oraweechan@orawees-MacBook-Air.local>
2021-11-22 07:45:34 -0800

    edit signup component

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,37 +1,54 @@
-import { Container, Col, Row } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { Container } from "react-bootstrap";
+import React, { useState } from "react";
 
 function SignUp() {
+  const [accountInfo, setAccountInfo] = useState({ email: "", name: "",password:"" });
+
   return (
     <>
+      <h1>Sign Up</h1>
       <Container>
-        <Form>
-          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-            <Form.Label column sm="2">
-              Email
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control
-                plaintext
-                readOnly
-                defaultValue="email@example.com"
-              />
-            </Col>
-          </Form.Group>
-
-          <Form.Group
-            as={Row}
-            className="mb-3"
-            controlId="formPlaintextPassword"
-          >
-            <Form.Label column sm="2">
-              Password
-            </Form.Label>
-            <Col sm="10">
-              <Form.Control type="password" placeholder="Password" />
-            </Col>
-          </Form.Group>
-        </Form>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              className="outlined-textarea"
+              label="Email"
+              placeholder="Email"
+              multiline
+            />
+            <TextField
+              className="outlined-textarea"
+              label="Full Name"
+              placeholder="Full Name"
+              multiline
+            />
+            <TextField
+              className="outlined-textarea"
+              label="Username"
+              placeholder="Username"
+              multiline
+            />
+            <TextField
+              className="outlined-password-input"
+              type="password"
+              label="Password"
+              autoComplete="current-password"
+            />
+            <Button variant="outlined" size="large">
+              Sign up
+            </Button>
+          </div>
+        </Box>
       </Container>
     </>
   );
