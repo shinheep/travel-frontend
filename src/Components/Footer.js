63fc061421fe954dfccfb087bec536742645ee31
Daniel Lee <daniellee@Daniels-MacBook-Air.local>
2021-11-22 09:50:47 -0800

    updated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,19 +1,26 @@
-import { Nav } from "react-bootstrap";
+import { Nav } from 'react-bootstrap';
 
 function Footer() {
 	return (
 		<div className='Footer'>
-			<Nav variant='pills' defaultActiveKey='/home'>
+			<Nav
+				className='justify-content-center'
+				variant='pills'
+				defaultActiveKey='/home'>
 				<Nav.Item>
-					<Nav.Link href='/home'>Active</Nav.Link>
+					<Nav.Link /*href='/home'*/ eventKey='link-1'> Option 1</Nav.Link>
 				</Nav.Item>
 				<Nav.Item>
-					<Nav.Link eventKey='link-1'>Option 2</Nav.Link>
+					<Nav.Link /*href='/home'*/ eventKey='link-2'> Option 2</Nav.Link>
 				</Nav.Item>
 				<Nav.Item>
-					<Nav.Link eventKey='disabled' disabled>
-						Disabled
-					</Nav.Link>
+					<Nav.Link /*href='/home'*/ eventKey='link-3'> Option 3</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link /*href='/home'*/ eventKey='link-4'> Option 4</Nav.Link>
+				</Nav.Item>
+				<Nav.Item>
+					<Nav.Link /*href='/home'*/ eventKey='link-5'> Option 5</Nav.Link>
 				</Nav.Item>
 			</Nav>
 		</div>
