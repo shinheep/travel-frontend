6c6d4a3420ea6027b56f0df461b78f9d9be8419f
orawee chan <oraweechan@orawees-MacBook-Air.local>
2021-11-23 00:31:43 -0800

    edit login and sign up styling

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,65 +1,99 @@
 import { Container, Col, Row } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import React, { useState } from "react";
+
 const linkStyle = {
-	textDecoration: 'none',
-	color: 'white',
+  textDecoration: "none",
+  color: "white",
 };
 
 function Login() {
-  return (
-		<>
-			<Container>
-				<Row className='justify-content-md-center'>
-					<Col sm={3}>
-						<h1>Login</h1>
-						<Form>
-							<Form.Group
-								as={Row}
-								className='mb-3'
-								controlId='formPlaintextEmail'>
-								<Form.Label column sm='4'>
-									Email
-								</Form.Label>
-								<Col sm='12'>
-									<Form.Control placeholder='Email' />
-								</Col>
-							</Form.Group>
+  const [signIn, setSignIn] = useState({
+    email: "",
+    password: "",
+  });
 
-							<Form.Group
-								as={Row}
-								className='mb-3'
-								controlId='formPlaintextPassword'>
-								<Form.Label column sm='4'>
-									Password
-								</Form.Label>
-								<Col sm='12'>
-									<Form.Control type='password' placeholder='Password' />
-								</Col>
-							</Form.Group>
-							<Button
-								style={{ backgroundColor: '#05668d' }}
-								variant='contained'
-								size='large'>
-								Login
-							</Button>
-						</Form>
-            <br />
-            <p>OR</p>
-						<Button
-							style={{ backgroundColor: '#05668d' }}
-							variant='contained'
-							size='large'>
-							<Link style = {linkStyle} to='/signup'>Sign up</Link>
-						</Button>
-					</Col>
-				</Row>
-				
-			</Container>
-		</>
-	);
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    const value = event.target.value;
+    const name = event.target.name;
+    const copy = Object.assign({}, signIn);
+    copy[name] = value;
+    setSignIn(copy);
+  };
+
+  return (
+    <>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col sm={3}>
+            <h1>Sign In</h1>
+            <Box
+              justifyContent="center"
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 2, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                className="outlined-textarea"
+                label="Email"
+                placeholder="Email"
+                multiline
+                onChange={handleChange}
+                value={signIn.email}
+                name="email"
+              />
+
+              <TextField
+                className="outlined-password-input"
+                type="password"
+                label="Password"
+                autoComplete="current-password"
+                onChange={handleChange}
+                value={signIn.password}
+                name="password"
+              />
+              <div className= "buttons">
+              <Button
+              className="d-flex justify-content-end"
+                style={{ backgroundColor: "#05668d" }}
+                type="submit"
+                variant="contained"
+                size="large"
+              >
+                Sign in
+              </Button>
+             
+              <p>OR</p>
+              <Button
+                style={{ backgroundColor: "#05668d" }}
+                variant="contained"
+                size="large"
+              >
+                <Link style={linkStyle} to="/signup">
+                  Sign up
+                </Link>
+              </Button>
+
+              </div>
+
+              
+            </Box>
+          </Col>
+        </Row>
+      </Container>
+
+    </>
+  );
 }
 
 export default Login;
