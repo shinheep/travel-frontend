a5459d026127c525bdf2a3011c78f7cc52aa7f54
Shinhee Park <shinhee.park777@gmail.com>
2022-01-25 09:38:33 -0800

    Fixed Explore page bug

--- src/Components/Explore/Explore.js
+++ src/Components/Explore/Explore.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
 import "./explore.css";
@@ -33,62 +32,6 @@ function Explore(props) {
       {posts}
     </div>
   );
-=======
-import React, { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
-import './explore.css';
-
-function Explore() {
-	const [exploreData, setExploreData] = useState([]);
-	// const posts = []
-
-	// const makeApiCall = () => {
-	//     fetch("https://travelgram-app-heroku.herokuapp.com/users/")
-	//     .then((res) => res.json())
-	//     .then((data) => setExploreData(data.user))
-	// }
-
-	const tempApiCall = () => {
-		//change this later to get all feeds not just danielmlee92
-		fetch('https://travelgram-app-heroku.herokuapp.com/users/daniel123')
-			.then((res) => res.json())
-			.then((data) => setExploreData(data.user.posts));
-	};
-
-	useEffect(() => {
-		tempApiCall();
-	}, []);
-
-	const posts =
-		exploreData &&
-		exploreData.map((post) => {
-			return (
-				<div className='postCard'>
-					<Card>
-						<Card.Img className='postImg' variant='top' src={post.img} />
-						<Card.Body>
-							{/* <Card.Link onClick={() => likedPost(post._id)} key={post._id}>
-                  <i class="far fa-heart"></i>
-                </Card.Link> */}
-							{/* <Card.Text>{post.likes[0].likes > 0 ? post.likes[0].likes + " likes": " "} </Card.Text> */}
-							<Card.Title>{post.username}</Card.Title>
-							<Card.Text>
-								<i class='fas fa-map-marker-alt'></i> {post.location} <br />
-								Price: {post.price} <br />
-								{post.caption}
-							</Card.Text>
-						</Card.Body>
-					</Card>
-				</div>
-			);
-		});
-	return (
-		<div className='Explore'>
-			<h1 style={{ textAlign: 'center' }}>Discover your next destination</h1>
-			{posts}
-		</div>
-	);
->>>>>>> 55237ff78fbc4952255e078c90e8141161e0ae74
 }
 
 export default Explore;
